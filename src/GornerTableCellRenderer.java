d8fbc25eadab5d3802df41f7f8357d337dca50e8
Yahor <zloyman212906@gmail.com>
2025-10-21 18:51:27 +0300

    - Добавлена логика для отображения checkbox в третьем столбце

--- src/GornerTableCellRenderer.java
+++ src/GornerTableCellRenderer.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
@@ -13,6 +14,7 @@ import javax.swing.table.TableCellRenderer;
 public class GornerTableCellRenderer implements TableCellRenderer {
     private JPanel panel = new JPanel();
     private JLabel label = new JLabel();
+    private JCheckBox checkBox = new JCheckBox();
     private String needle = null;
     private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
 
@@ -26,19 +28,41 @@ public class GornerTableCellRenderer implements TableCellRenderer {
 
         panel.add(label);
         panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        // Настройка checkbox для третьего столбца
+        checkBox.setHorizontalAlignment(JCheckBox.CENTER);
     }
 
     public Component getTableCellRendererComponent(JTable table, Object value,
                                                    boolean isSelected, boolean hasFocus, int row, int col) {
-        String formattedDouble = formatter.format(value);
-        label.setText(formattedDouble);
 
-        if (col == 1 && needle != null && needle.equals(formattedDouble)) {
-            panel.setBackground(Color.RED);
+        if (col == 2) {
+            checkBox.setSelected((Boolean) value);
+
+            // Установка цвета фона
+            if (isSelected) {
+                checkBox.setBackground(table.getSelectionBackground());
+            } else {
+                checkBox.setBackground(table.getBackground());
+            }
+
+            return checkBox;
         } else {
-            panel.setBackground(Color.WHITE);
+            // Для первых двух столбцов - обычное отображение
+            String formattedDouble = formatter.format(value);
+            label.setText(formattedDouble);
+
+            if (col == 1 && needle != null && needle.equals(formattedDouble)) {
+                panel.setBackground(Color.RED);
+            } else {
+                if (isSelected) {
+                    panel.setBackground(table.getSelectionBackground());
+                } else {
+                    panel.setBackground(table.getBackground());
+                }
+            }
+            return panel;
         }
-        return panel;
     }
 
     public void setNeedle(String needle) {
